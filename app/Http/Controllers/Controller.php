906041d40a6026cc00d0fa1a4c42c2f54d83bbe3
DESKTOP-5JO9TMG\Rifdah <60900119009@uin-alauddin.ac.id>
2021-12-01 11:25:46 +0800

    Ubah saja

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+//Perubahan Saja
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
