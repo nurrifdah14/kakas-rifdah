c47da3b18ce4f7220f1cbf45a1f54ab0cb7a8426
DESKTOP-5JO9TMG\Rifdah <60900119009@uin-alauddin.ac.id>
2021-12-01 11:32:18 +0800

    perubahan

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-//Perubahan Dulu
+//Perubahan Dulu..
 
 class Controller extends BaseController
 {
