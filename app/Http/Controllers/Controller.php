efef7945d7c464ec4fa2db2c364e5c54bd54c18d
DESKTOP-5JO9TMG\Rifdah <60900119009@uin-alauddin.ac.id>
2021-12-01 12:09:21 +0800

    push dulu

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-//Perubahan Dulu..
+//Perubahan controller
 
 class Controller extends BaseController
 {
